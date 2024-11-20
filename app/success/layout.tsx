1e540df843f910ca4f0b64490fd6a482fd46c6bb
Steven Zimmer <webdevzim@gmail.com>
2024-11-20 06:05:53 -0800

    noindex success page

--- app/success/layout.tsx
+++ app/success/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+    robots: {
+      index: false,
+    },
+  };
+
 export default function layout({children}: {children: React.ReactNode}) {
     return <div className="min-h-dvh flex  justify-center ">{children}</div>;
 }
