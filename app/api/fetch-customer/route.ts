ccdeee9c7f34dec485971431da417c66c7de36f0
Steven Zimmer <webdevzim@gmail.com>
2026-03-27 10:00:55 -0700

    add disabled state

--- app/api/fetch-customer/route.ts
+++ app/api/fetch-customer/route.ts
@@ -1,65 +1,44 @@
-import { stripe } from "@/utils/stripe";
+import {stripe} from "@/utils/stripe";
 
-import { NextRequest, NextResponse } from "next/server";
-// import {getServerSession} from "next-auth/next";
-// import {authOptions} from "../auth/[...nextauth]/route";
-
-// import {prisma} from "@/lib/prisma";
-import { AddCartType } from "@/types/Cart";
-import { metadata } from "@/app/layout";
-
-// type Item = {
-//     name: string;
-//     description: string;
-//     unit_amount: number;
-//     image: string;
-//     quantity: number;
-// };
-
-// const calculateOrderAmount = (items: Item[]) => {
-//     const totalPrice = items.reduce((acc, item) => {
-//         return acc + item.unit_amount! * item.quantity!;
-//     }, 0);
-//     return totalPrice;
-// };
+import {NextResponse} from "next/server";
 
 export async function POST(req: Request) {
-  const { email, name } = await req.json();
+    const {email, name} = await req.json();
 
-  try {
-    const searchCustomers = await stripe.customers.search({
-      query: `email:\'${email}\'`,
-    });
+    try {
+        const searchCustomers = await stripe.customers.search({
+            query: `email:\'${email}\'`,
+        });
 
-    let newCustomer;
+        let newCustomer;
 
-    let isNewCustomer = false;
+        let isNewCustomer = false;
 
-    if (!searchCustomers.data.length) {
-      isNewCustomer = true;
-      newCustomer = await stripe.customers.create({
-        email,
-        name,
-      });
+        if (!searchCustomers.data.length) {
+            isNewCustomer = true;
+            newCustomer = await stripe.customers.create({
+                email,
+                name,
+            });
+        }
+
+        return NextResponse.json(
+            {
+                customer: newCustomer || searchCustomers.data[0],
+                isNewCustomer,
+            },
+            {
+                status: 200,
+            },
+        );
+    } catch (error) {
+        return NextResponse.json(
+            {
+                error: error instanceof Error && error.message,
+            },
+            {
+                status: 500,
+            },
+        );
     }
-
-    return NextResponse.json(
-      {
-        customer: newCustomer || searchCustomers.data[0],
-        isNewCustomer,
-      },
-      {
-        status: 200,
-      },
-    );
-  } catch (error) {
-    return NextResponse.json(
-      {
-        error: error instanceof Error && error.message,
-      },
-      {
-        status: 500,
-      },
-    );
-  }
 }
